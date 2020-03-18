37c2a3807ddddb47dcfa7c22b72c89fc9ce55b66
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-03-18 19:25:58 -0400

    Update xen_installer.sh

--- xen_installer.sh
+++ xen_installer.sh
@@ -3,17 +3,12 @@ echo "Xen Hypervisor 4.11 amd64 autoconfig & installer v1.1BETA by https://githu
 echo "Please enter your password to continue...";
 sudo clear;
 read -p "Do you want to install the xen dependencies and other software from apt? [ Y ]" ready
-echo "Xen Hypervisor 4.11 amd64 autoconfig & installer v1.1BETA by https://github.com/independentcod";
-echo "Please enter your password to continue...";
-sudo clear;
-read -p "Do you want to install the xen dependencies and other software from apt? [ Y ]" ready
 case $ready in
 	Y) echo "Installing the necessary, please wait..." && sudo xterm -e sudo apt update && sudo apt install xen-hypervisor-4.11-amd64 xen-hypervisor-common xen-tools gparted xterm vinagre -y &
 esac
 read -p "Do you want to launch the download of Windows-server-2019-evaluation.iso from Microsoft website? [ Y ]" dl
 case $dl in
 	 Y) xterm -e wget -O windows-server-2019-evaluation.iso https://is.gd/winserver2019 &
-esac
 read -p "Do you want to edit your configurations? [ Y ]" edit
 case $edit in 
 	Y) wget -O xen.cfg https://pastebin.com/raw/uDsnswQg;
@@ -22,7 +17,6 @@ if read -p "Give a name to your Virtual Machine: [eg: WindowsServer]" name; 
 read -p "Do you know in which partition you want to install your VM [ N ]" dl;
 case $dl in N) echo "First you have to make sure you have enough space to make a new volume group";
 	sudo gparted;
-esac
 echo "Now you must tell this installer where the volume group will be installed";
 if read -p "Where the volume group should be located? (eg. /dev/sda3)" loc;
 	then sed -i~ -e "s|/dev/sda6|server => \'${loc}\',|g" xen.cfg;
@@ -33,92 +27,15 @@ if read -p "What size the logical volume should have? (Recommend: 20G minimum)" 
 fi
 if read -p "How much RAM, in MB, should your VM use? (Recommend: 2048)" maxmem;
 	then sed -i~ -e "s|2048|server => \'${maxmem}\',|g" xen.cfg;
-fi 
-fi
 read -p "Time to reboot your machine are you ready? (Make sure download is finished) [ Y ]" ready;
 case $ready
 	in Y) echo "Once you are booted make sure you sre booting with xen and rerun this script for the second part of the setup. Rebooting in 15 seconds..." && sleep 15 && sudo reboot now;;
-esac
-esac
 read -p "ARE YOU READY FOR THE 2ND PART OF THE SCRIPT? [ Y ]" ready
-case $ready
 	in Y) echo "To connect to the virtual machine the protocol is SPICE and address is 127.0.0.1:1" && sudo xl create xen.cfg && vinagre sudo brctl addbr xenbr0 && sudo ifconfig xenbr0 up;;  
-esac
-case $ready in
-	Y) echo "Installing the necessary, please wait..." && sudo xterm -e sudo apt update && sudo apt install xen-hypervisor-4.11-amd64 xen-hypervisor-common xen-tools gparted xterm vinagre -y &
-esac
-read -p "Do you want to launch the download of Windows-server-2019-evaluation.iso from Microsoft website? [ Y ]" dl
-case $dl in
 	Y) xterm -e wget -O windows-server-2019-evaluation.iso https://is.gd/winserver2019 &
-esac
-read -p "Do you want to edit your configurations? [ Y ]" edit
 case $edit in 
-	Y) wget -O xen.cfg https://pastebin.com/raw/uDsnswQg;
 if read -p "Give a name to your Virtual Machine: [eg: WindowsServer]" name; 
 	then sed -i~ -e "s|WindowsServer|server => \'${name}\',|g" xen.cfg; 
-read -p "Do you know in which partition you want to install your VM [ N ]" dl;
-case $dl in N) echo "First you have to make sure you have enough space to make a new volume group";
-	sudo gparted;
-esac
-echo "Now you must tell this installer where the volume group will be installed";
-if read -p "Where the volume group should be located? (eg. /dev/sda3)" loc;
-	then sed -i~ -e "s|/dev/sda6|server => \'${loc}\',|g" xen.cfg;
-sudo vgcreate ${name} ${loc};
-fi 
-if read -p "What size the logical volume should have? (Recommend: 20G minimum)" lv;
-	then sudo lvcreate -L ${lv} ${name};
-fi
-if read -p "How much RAM, in MB, should your VM use? (Recommend: 2048)" maxmem;
-	then sed -i~ -e "s|2048|server => \'${maxmem}\',|g" xen.cfg;
-fi 
-fi
-read -p "Time to reboot your machine are you ready? (Make sure download is finished) [ Y ]" ready;
-case $ready
-	in Y) echo "Once you are booted make sure you sre booting with xen and rerun this script for the second part of the setup. Rebooting in 15 seconds..." && sleep 15 && sudo reboot now;;
-esac
-esac
-read -p "ARE YOU READY FOR THE 2ND PART OF THE SCRIPT? [ Y ]" ready
-case $ready
-	in Y) echo "To connect to the virtual machine the protocol is SPICE and address is 127.0.0.1:1" && sudo xl create xen.cfg && vinagre sudo brctl addbr xenbr0 && sudo ifconfig xenbr0 up;;  
-esac#!/usr/bin/bash
-echo "Xen Hypervisor 4.11 amd64 autoconfig & installer v1.1BETA by https://github.com/independentcod";
-echo "Please enter your password to continue...";
-sudo clear;
-read -p "Do you want to install the xen dependencies and other software from apt? [ Y ]" ready
-case $ready in
-	Y) echo "Installing the necessary, please wait..." && sudo xterm -e sudo apt update && sudo apt install xen-hypervisor-4.11-amd64 xen-hypervisor-common xen-tools gparted xterm vinagre -y &
-esac
-read -p "Do you want to launch the download of Windows-server-2019-evaluation.iso from Microsoft website? [ Y ]" dl
-case $dl in
-	Y) xterm -e wget -O windows-server-2019-evaluation.iso https://is.gd/winserver2019 &
-esac
-read -p "Do you want to edit your configurations? [ Y ]" edit
-case $edit in 
-	Y) wget -O xen.cfg https://pastebin.com/raw/uDsnswQg;
-if read -p "Give a name to your Virtual Machine: [eg: WindowsServer]" name; 
-	then sed -i~ -e "s|WindowsServer|server => \'${name}\',|g" xen.cfg; 
-read -p "Do you know in which partition you want to install your VM [ N ]" dl;
-case $dl in N) echo "First you have to make sure you have enough space to make a new volume group";
-	sudo gparted;
-esac
-echo "Now you must tell this installer where the volume group will be installed";
-if read -p "Where the volume group should be located? (eg. /dev/sda3)" loc;
-	then sed -i~ -e "s|/dev/sda6|server => \'${loc}\',|g" xen.cfg;
-sudo vgcreate ${name} ${loc};
-fi 
-if read -p "What size the logical volume should have? (Recommend: 20G minimum)" lv;
-	then sudo lvcreate -L ${lv} ${name};
-fi
-if read -p "How much RAM, in MB, should your VM use? (Recommend: 2048)" maxmem;
-	then sed -i~ -e "s|2048|server => \'${maxmem}\',|g" xen.cfg;
-fi 
-fi
-read -p "Time to reboot your machine are you ready? (Make sure download is finished) [ Y ]" ready;
-case $ready
-	in Y) echo "Once you are booted make sure you sre booting with xen and rerun this script for the second part of the setup. Rebooting in 15 seconds..." && sleep 15 && sudo reboot now;;
-esac
-esac
-read -p "ARE YOU READY FOR THE 2ND PART OF THE SCRIPT? [ Y ]" ready
-case $ready
-	in Y) echo "To connect to the virtual machine the protocol is SPICE and address is 127.0.0.1:1" && vinagre sudo brctl addbr xenbr0 && sudo ifconfig xenbr0 up && sudo xl create xen.cfg;;  
+fi  
+   in Y) echo "To connect to the virtual machine the protocol is SPICE and address is 127.0.0.1:1" && vinagre && sudo brctl addbr xenbr0 && sudo ifconfig xenbr0 up && sudo xl create xen.cfg;;   
 esac
